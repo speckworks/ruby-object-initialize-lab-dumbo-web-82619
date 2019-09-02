3380e980915b006c72f0af29b42f7922ba2d3827
John Karrell Speck <speckworks@gmail.com>
2019-09-02 21:07:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   attr_accessor :name, :breed 
   
-  def initialize(name = "mutt", breed) 
+  def initialize(name = "Mutt", breed = "Mutt") 
     @name = name  
     @breed = breed
   end
